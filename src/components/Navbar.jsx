a425a0edacbb983f1e29d330887759250c32af69
D-i-m-i <demetristz@gmail.com>
2026-03-22 09:39:01 +0700

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
             isActive ? 'text-blue-500' : 'text-black'
           }
         >
-          Contacts
+          Contact
         </NavLink>
       </nav>
     </header>
